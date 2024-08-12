a263a8c406da964f247c8e6c203e42f4bfacaf90
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-08-13 06:41:24 +0700

    feat: home page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,7 +3,16 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Welcome to Aksamedia</h1>
+      <section className="flex flex-col justify-center items-center gap-4 bg-baseblue-100 min-h-screen">
+        <h1 className="m-7 font-extrabold text-7xl text-white">Welcome to Aksastock</h1>
+        <p className="text-white text-xl">Aksastock is a stock management application.</p>
+
+        <div className="bg-white mt-4 px-7 py-3 rounded-lg">
+          <a href="/login" className="text-blue-500">
+            Sign in here
+          </a>
+        </div>
+      </section>
     </>
   );
 }
