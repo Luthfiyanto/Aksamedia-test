d60ba46e3a992f4052f5d03cfd18e905cb01aaf6
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-08-13 08:35:38 +0700

    style: fix size

--- src/components/layouts/SidebarEdit.jsx
+++ src/components/layouts/SidebarEdit.jsx
@@ -6,7 +6,7 @@ export default function SidebarEdit() {
   const { productId, productName, description, productPrice, productStock, productImage, setProductId, setProductName, setDescription, setProductPrice, setProductStock, setProductImage } = useContext(ProductContext);
   return (
     <>
-      <section className="flex flex-col gap-7 bg-white shadow-md p-5 rounded-e-md w-4/5 min-h-screen">
+      <section className="flex flex-col gap-4 bg-white shadow-md p-5 rounded-e-md w-4/5 min-h-screen">
         <h1 className="my-6 text-3xl text-baseblue-300">Manage Product</h1>
         <InputText type={"text"} value={productId} trigger={setProductId} dataName={"Product ID"} />
         <InputText type={"text"} value={productName} trigger={setProductName} dataName={"Product Name"} />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ export default function Login() {
       <section className="flex flex-col justify-center items-center min-h-screen">
         <h1 className="my-3 font-bold text-3xl text-baseblue-300">Welcome to Aksastore</h1>
         <h3 className="my-3 font-medium text-baseblue-300 text-lg">Sign in</h3>
-        <form className="grid grid-cols-1 shadow-md m-6 p-6 w-1/2 md:w-1/3 h-64 text-zinc-500">
+        <form className="grid grid-cols-1 shadow-md m-6 p-6 w-2/3 md:w-1/3 h-64 text-zinc-500">
           <label htmlFor="email" className="my-3 h-10">
             <input type="email" id="email" name="email" placeholder="" className="border-b-2 focus:border-baseblue-100 w-full transition-all ease-in-out outline-none peer/email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <p className="relative -top-12 peer-focus/email:-top-12 peer-placeholder-shown/email:-top-7 text-sm peer-focus/email:text-sm peer-focus/email:text-baseblue-100 peer-placeholder-shown/email:text-base transition-all ease-in-out">
