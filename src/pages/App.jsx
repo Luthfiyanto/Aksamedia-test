f99e05f09692e56ccaa4910169a37cfee2ff81bb
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-08-13 07:47:09 +0700

    style: signin button

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,11 +5,9 @@ function App() {
         <h1 className="p-7 font-extrabold text-6xl text-white sm:text-7xl sm:text-center">Welcome to Aksastock</h1>
         <p className="p-7 text-white text-xl sm:text-center">Aksastock is a stock management application.</p>
 
-        <div className="bg-white m-6 mt-4 px-7 py-3 rounded-lg">
-          <a href="/login" className="text-blue-500">
-            Sign in here
-          </a>
-        </div>
+        <a href="/login" className="text-blue-500">
+          <div className="bg-white m-6 mt-4 px-7 py-3 rounded-lg font-medium">Sign in here</div>
+        </a>
       </section>
     </>
   );
