8883642d4c3a9a52cbaeb24dd5830505e8f58f9a
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-08-13 07:28:31 +0700

    style: home page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-
 function App() {
   return (
     <>
-      <section className="flex flex-col justify-center items-center gap-4 bg-baseblue-100 min-h-screen">
-        <h1 className="m-7 font-extrabold text-7xl text-white">Welcome to Aksastock</h1>
-        <p className="text-white text-xl">Aksastock is a stock management application.</p>
+      <section className="flex flex-col justify-center sm:items-center gap-2 sm:gap-4 bg-baseblue-100 min-h-screen">
+        <h1 className="p-7 font-extrabold text-6xl text-white sm:text-7xl sm:text-center">Welcome to Aksastock</h1>
+        <p className="p-7 text-white text-xl sm:text-center">Aksastock is a stock management application.</p>
 
-        <div className="bg-white mt-4 px-7 py-3 rounded-lg">
+        <div className="bg-white m-6 mt-4 px-7 py-3 rounded-lg">
           <a href="/login" className="text-blue-500">
             Sign in here
           </a>
